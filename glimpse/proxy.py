7af815a9f1230fa45c07bcaf87adfc0878a2296c
Derrick J. Wippler <thrawn01@gmail.com>
2014-10-23 08:11:32 -0500

    glance server to proxy for is now a config option

--- glimpse/proxy.py
+++ glimpse/proxy.py
@@ -1,7 +1,20 @@
+from oslo.log._i18n import _
+from oslo.config import cfg
 import falcon
+
+proxy_opts = [
+    cfg.StrOpt('glance_server', default='http://glance:9292',
+               help=_('The glance server we are a proxy for. '
+                      'All requests that are not matched by our'
+                      ' routes are proxied to this server'))
+]
+
+CONF = cfg.CONF
+CONF.register_opts(proxy_opts)
 
 
 class ProxySink(object):
     def __call__(self, req, resp, **kwargs):
-        resp.status = falcon.HTTP_200  # This is the default status
-        resp.body = ('\nThis is a proxy request for %s\n' % kwargs)
+        resp.status = falcon.HTTP_302
+        resp.set_header('Location', CONF.glance_server + req.path
+                        + '?' + req.query_string)
